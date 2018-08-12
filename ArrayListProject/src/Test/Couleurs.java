d878e9590c06b5c831fa683957706216e23648db
pierrepoulouin <pierrepoulouin@gmail.com>
2018-08-12 11:02:36 +0200

    Creation de la classe Couleurs

---
+++ ArrayListProject/src/Test/Couleurs.java
@@ -0,0 +1,22 @@
+package Test;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Couleurs {
+
+	public static void main(String[] args) {
+		//création de l'instance couleurs de la classe ArrayList
+		List<String> couleurs = new ArrayList<>();
+		
+		//Ajout de valeurs avec la methode add de la class ArrayList
+		couleurs.add("Rouge");
+		couleurs.add("Bleu");
+		couleurs.add("Rouge");
+		
+		System.out.print(couleurs);
+		
+
+	}
+
+}
