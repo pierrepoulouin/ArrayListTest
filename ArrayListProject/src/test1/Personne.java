2108971788e3aaff586349f320a3080a41a2e212
pierrepoulouin <pierrepoulouin@gmail.com>
2018-08-29 10:46:43 +0200

    Création de la Class

---
+++ ArrayListProject/src/test1/Personne.java
@@ -0,0 +1,44 @@
+package test1;
+
+public class Personne {
+
+	String nom;
+	String prenom;
+	String email;
+	int age;
+	String telephone;
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+
+}
